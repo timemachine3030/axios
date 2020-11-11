126109dafb50d49c3390f50cc3a6f4a5dc54de1d
pilot <timemachine3030@gmail.com>
2020-11-10 22:14:07 -0800

    Slow down the transfer

--- test/unit/regression/3396.js
+++ test/unit/regression/3396.js
@@ -2,19 +2,31 @@ const axios = require('../../../index');
 const package = require('../../../package.json');
 const http = require('http');
 const fs = require('fs');
+const { Transform } = require('stream');
+
+
 
 let n = 1;
 let total = 0;
 
 describe('3396', () => {
   before((done) => {
-    const requestHandler = (request, response) => {
-      setTimeout(() => {
-        fs.createReadStream(__dirname + '/../../../package-lock.json')
-        .pipe(response);
-      }, 250);
-    }
-    const server = http.createServer(requestHandler)
+    const server = http.createServer((request, response) => {
+      // simulate slow connection:
+      const throttler = new Transform({
+        transform(chunk, encoding, send) {
+          setTimeout(() => {
+            send(null, chunk);
+          }, 100);
+        }
+      });
+
+      let fh = fs.createReadStream(__dirname + '/../../../package-lock.json', 'utf-8');
+      fh.pipe(throttler).pipe(response)
+        .on('end', () => {
+          fh.close();
+        });
+    })
     server.listen(3000, done);
   });
   it('crashes node.exe', async () => {
@@ -28,6 +40,7 @@ describe('3396', () => {
         .catch(err => { console.error("ERROR: " + err.message); process.exit() })
     } while (total < 3000000000); // Transfer 3Gb.	  
     console.log(`Moved ${total.toLocaleString()} bytes no error`);
+    server.close();
   });
 });
 
