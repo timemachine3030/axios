92b3a65284737da0fd5111fef13b6e56afa9b8b7
Pilot <timemachine@ctrl-c.club>
2021-05-20 23:47:03 -0400

    Removes the use of `createElement`
     - Add extra test for relative paths

--- test/specs/helpers/isURLSameOrigin.spec.js
+++ test/specs/helpers/isURLSameOrigin.spec.js
@@ -8,4 +8,8 @@ describe('helpers::isURLSameOrigin', function () {
   it('should detect different origin', function () {
     expect(isURLSameOrigin('https://github.com/axios/axios')).toEqual(false);
   });
+  
+  it('should work with relative paths', function () {
+    expect(isURLSameOrigin('/local-path')).toEqual(true);
+  });
 });
